20af88900a3d4d458934889f5dd8307495d33e48
Viktor Zhurbin <vzhurbin@gmail.com>
2025-09-01 22:24:59 +0200

    bundle analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import mdx from "@mdx-js/rollup";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,10 +13,26 @@ export default defineConfig({
       }),
     },
     react(),
+    visualizer({
+      filename: "dist/stats.html",
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+    }),
   ],
   resolve: {
     alias: {
       "@": "/src",
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom", "react-dom/client", "scheduler"],
+          router: ["react-router-dom"],
+        },
+      },
     },
   },
 });
