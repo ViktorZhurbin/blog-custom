41831c915837dadc9d1766d22e7e030d9e765b50
Viktor Zhurbin <vzhurbin@gmail.com>
2025-09-05 17:21:59 +0200

    format

--- src/constants.ts
+++ src/constants.ts
@@ -1,16 +1,16 @@
 export const PATHS = {
-  CONTENT_DIR: 'content',
-  POSTS_DIR: 'content/posts',
-  GENERATED_DIR: 'src/generated',
-  POSTS_INDEX: 'src/generated/posts.json'
-} as const
+  CONTENT_DIR: "content",
+  POSTS_DIR: "content/posts",
+  GENERATED_DIR: "src/generated",
+  POSTS_INDEX: "src/generated/posts.json",
+} as const;
 
 export const PATTERNS = {
-  MDX_FILES: 'content/posts/**/*.mdx',
-  MDX_FILES_REGEX: /content\/posts\/.*\.mdx$/
-} as const
+  MDX_FILES: "content/posts/**/*.mdx",
+  MDX_FILES_REGEX: /content\/posts\/.*\.mdx$/,
+} as const;
 
 export const COLORS = {
-  PURPLE: '\x1b[35m',
-  RESET: '\x1b[0m'
-} as const
+  PURPLE: "\x1b[35m",
+  RESET: "\x1b[0m",
+} as const;
