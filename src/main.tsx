c2bb9e4c3b8b6a83f63ab280fb453d5f6cabc591
Viktor Zhurbin <vzhurbin@gmail.com>
2025-09-06 11:21:34 +0200

    use static postsIndex import

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { hydrate, prerender as ssr } from "preact-iso";
+import postsIndex from "@/generated/posts.json";
 
 import App from "./App";
 
@@ -7,5 +8,20 @@ if (typeof window !== "undefined") {
 }
 
 export async function prerender(data) {
+  // For post pages, dynamically import the content
+  if (data.url?.startsWith("/posts/")) {
+    const slug = data.url.split("/")[2];
+    const post = postsIndex.find((p) => p.slug === slug);
+
+    if (post) {
+      const { default: MDXContent } = await import(
+        `../content/posts/${slug}.mdx`
+      );
+
+      // Pass the component via globalThis or data
+      globalThis[slug] = MDXContent;
+    }
+  }
+
   return await ssr(<App {...data} />);
 }
